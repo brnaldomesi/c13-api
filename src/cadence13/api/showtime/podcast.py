113e1214cc77eb078dca99c546e62be891146f9d
Ken Lenga <kennethlenga@gmail.com>
2019-06-18 04:55:00 +0000

    Add pagination to /podcasts

--- src/cadence13/api/showtime/podcast.py
+++ src/cadence13/api/showtime/podcast.py
@@ -1,83 +1,20 @@
+from cadence13.api.util.logging import get_logger
 import cadence13.api.common.podcast as common_podcast
 import time
 import boto3
 
+logger = get_logger(__name__)
 
-def get_podcasts():
-    return common_podcast.get_podcasts()
+
+def get_podcasts(startAfter=None, endingBefore=None,
+                 limit=common_podcast.PODCASTS_LIMIT):
+    logger.info('{}, {}, {}'.format(startAfter, endingBefore, limit))
+    return common_podcast.get_podcasts(
+        startAfter, endingBefore, limit)
 
 
 def get_podcast(podcastGuid):
     return common_podcast.get_podcast(podcastGuid)
-    # return {
-    #     'guid': podcastGuid,
-    #     'slug': 'pod-save-america',
-    #     'title': 'Pod Save America',
-    #     'subtitle': None,
-    #     'summary': 'This is a summary of Pod Save America',
-    #     'copyright': 'Crooked Media',
-    #     'author': 'Crooked Media',
-    #     'websiteUrl': 'https://crooked.com/podcast-series/pod-save-america/',
-    #     'feedUrl': 'http://feeds.feedburner.com/pod-save-america',
-    #     'imageUrls': {
-    #         'original': 'https://megaphone.imgix.net/podcasts/454f50d8-0f0c-11e9-a73b-132c930842e3/image/5e963f8f8811e208b1a90c91507a0f7880c7b861bf81c290e44c193e891a507bbe99798dd7ed0cd795a4fede54560f167fa023553f66c29ff86bf9f8322c7f83.jpeg?ixlib=rails-2.1.2'
-    #     },
-    #     'ownerName': 'Crooked Media',
-    #     'ownerEmail': 'testemail@example.com',
-    #     'podcastType': 'EPISODIC',
-    #     'isExplicit': False,
-    #     'status': 'ACTIVE',
-    #     'subscriptionUrls': {
-    #         'applePodcasts': 'https://itunes.apple.com/podcast/id1192761536',
-    #         'spotify': 'https://open.spotify.com/show/5JGorGvdwljJHTl6wpMXN3',
-    #         'locked': ['spotify']
-    #     },
-    #     'socialMediaUrls': {
-    #         'facebook': 'https://www.facebook.com/podsaveamerica/',
-    #         'twitter': 'https://twitter.com/PodSaveAmerica',
-    #         'locked': ['facebook', 'twitter']
-    #     },
-    #     'staff': [
-    #         {
-    #             'guid': 'd858bc53-9c3b-4bb8-a163-c58df7800121',
-    #             'podcastGuid': podcastGuid,
-    #             'firstName': 'Jon',
-    #             'middleName': None,
-    #             'lastName': 'Favreau',
-    #             'imageUrls': {
-    #                 'original': 'https://content.production.cdn.art19.com/images/bf/fa/50/92/bffa5092-df8d-41d6-9ea1-02b70693f41d/3183fa58305f036c4a18fb6c86f18475eccb419143f43f005dc79c01d9dd77f07441389356575e508606d89fc47e4d8a9f058514380f5f8fe97e653b7daa7c96.jpeg'
-    #             }
-    #         },
-    #         {
-    #             'guid': '4ca17120-86d4-4474-bd5e-3e5d4d521947',
-    #             'podcastGuid': podcastGuid,
-    #             'firstName': 'Jon',
-    #             'middleName': None,
-    #             'lastName': 'Lovett',
-    #             'imageUrls': {
-    #                 'original': 'https://content.production.cdn.art19.com/images/9a/9d/b0/8d/9a9db08d-843c-4b5d-b910-ffa9ed5d9a45/dd651d867f85fc4fd6f3230d662df6cb2dd9858c7a5e973a40129cabb6f5e61022c7ea9f887d2bbfdbc4de8081d3c93d328ca9ae320b9a0d9e0eb2d95948f243.jpeg'
-    #             }
-    #         },
-    #         {
-    #             'guid': '6f93d51f-ac2a-4159-9430-1337e0324425',
-    #             'podcastGuid': podcastGuid,
-    #             'firstName': 'Dan',
-    #             'middleName': None,
-    #             'lastName': 'Pfeiffer',
-    #             'imageUrls': {}
-    #         }
-    #     ],
-    #     'tags': [
-    #         'foo',
-    #         'bar'
-    #     ],
-    #     'categories': [
-    #         'News & Politics'
-    #     ],
-    #     'locked': [
-    #         'title', 'summary'
-    #     ]
-    # }
 
 
 def update_podcast():
