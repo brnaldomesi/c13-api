20bc33b849fd9f0228d8e0c357b18da0aba9c574
brnaldomesi <brnaldomesi1@gmail.com>
2019-08-02 05:57:26 +0000

    Update Network collection APIs
    - Add podcastCount to each network
    - Change /networks/:id/podcasts to use the same schema and orm
      as the /podcasts API
    
    Implements WEB-245

--- src/cadence13/api/showtime/controller/network.py
+++ src/cadence13/api/showtime/controller/network.py
@@ -1,19 +1,46 @@
 from flask_jwt_extended import jwt_required
-from cadence13.db.tables import Network, Podcast, NetworkSeriesMap
-from cadence13.db.enums import PodcastStatus
+from sqlalchemy.sql.functions import count
+from marshmallow import fields, Schema, post_dump, pre_dump
+from cadence13.db.tables import Network
 from cadence13.api.util.db import db
-from cadence13.api.common.schema.db import PodcastSchema, NetworkSchema
+from cadence13.api.common.schema.db import NetworkSchema
 from cadence13.api.common.schema.api import ApiPodcastSchema
+from cadence13.api.common.db.table import ApiPodcast
+from cadence13.db.enums import PodcastStatus, NetworkStatus
+
+
+# Not sure if this should go into common.schema
+# since this might be the only place this is used.
+class ApiNetworkListSchema(Schema):
+    network = fields.Nested(NetworkSchema, data_key='Network')
+    podcast_count = fields.Integer(data_key='podcastCount')
+
+    @pre_dump(pass_many=False)
+    def pre_dump(self, data, many, **kwargs):
+        return {
+            'network': data[0],
+            'podcast_count': data[1]
+        }
+
+    @post_dump(pass_many=False)
+    def post_dump(self, data, many, **kwargs):
+        data['Network']['podcastCount'] = data['podcastCount']
+        return data['Network']
 
 
 @jwt_required
 def get_networks():
     total = (db.session.query(Network.id)
-             .filter_by(status='ACTIVE').count())
-    networks = (db.session.query(Network)
-                .filter_by(status='ACTIVE').all())
-    schema = NetworkSchema(many=True)
-    data = schema.dump(networks)
+             .filter_by(status=NetworkStatus.ACTIVE.name).count())
+    # FIXME: The network table is old and stores the status as a string
+    # and not enum. Eventually that should change.
+    rows = (db.session.query(Network, count(ApiPodcast.id).label('podcast_count'))
+            .join(ApiPodcast, Network.id == ApiPodcast.network_id)
+            .filter(ApiPodcast.status == PodcastStatus.ACTIVE)
+            .filter(Network.status == NetworkStatus.ACTIVE.name)
+            .group_by(Network).all())
+    schema = ApiNetworkListSchema(many=True)
+    data = schema.dump(rows)
     return {
         'total': total,
         'data': data
@@ -27,12 +54,10 @@ def create_network():
 
 @jwt_required
 def get_podcasts(networkId):
-    podcasts = (db.session.query(Podcast)
-                .join(NetworkSeriesMap, NetworkSeriesMap.series_id == Podcast.series_id)
-                .filter(Podcast.status == PodcastStatus.ACTIVE)
-                .filter(NetworkSeriesMap.network_id == networkId)
+    podcasts = (db.session.query(ApiPodcast)
+                .filter(ApiPodcast.status == PodcastStatus.ACTIVE)
+                .filter(ApiPodcast.network_id == networkId)
                 .all())
-    schema = PodcastSchema(many=True)
+    schema = ApiPodcastSchema(many=True)
     result = schema.dump(podcasts)
     return result
-
